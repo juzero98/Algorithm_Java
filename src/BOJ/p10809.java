3982f83b64da60aec0e2bada4b9ce54767570dcc
PARKJUYOUNG <juzero98@naver.com>
2021-06-14 17:48:26 +0900

    solve BOJ p10809-알파벳 찾기

---
+++ src/BOJ/p10809.java
@@ -0,0 +1,22 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class p10809 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		String s = br.readLine();
+		br.close();
+		
+		for(char c = 'a'; c <= 'z'; c++)
+			bw.append(s.indexOf(c) + " ");
+		
+		bw.close();
+	}
+}
