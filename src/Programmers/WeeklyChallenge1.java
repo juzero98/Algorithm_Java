00b292cf638b0ae707a22a5f98289f1a5ae8b1e0
PARKJUYOUNG <juzero98@naver.com>
2021-08-05 14:55:09 +0900

    solve Programmers level1-부족한 금액 계산하기

---
+++ src/Programmers/WeeklyChallenge1.java
@@ -0,0 +1,22 @@
+package Programmers;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class WeeklyChallenge1 {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.append(solve(3, 20, 4) + "");
+		bw.close();
+	}
+
+	public static long solve(int price, int money, int count) {
+		long total = price * (count * ((long)count + 1) / 2);
+		
+		long answer = money >= total ? 0 : total - money;
+
+		return answer;
+	}
+}
