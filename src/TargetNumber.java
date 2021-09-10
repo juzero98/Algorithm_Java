6f564d8f0dedb702db904b6883c8833927ca073e
PARKJUYOUNG <juzero98@naver.com>
2021-09-11 02:31:37 +0900

    solve Programmers level2-타겟넘버

---
+++ src/TargetNumber.java
@@ -0,0 +1,25 @@
+
+// DFS/BFS - Å¸°Ù ³Ñ¹ö
+public class TargetNumber {
+	
+	public static void main(String[] args) {
+		int[] numbers = {1,1,1,1,1};
+		int target = 3;
+	
+		int answer = dfs(numbers, target, 0, 0);
+		
+		System.out.println(answer);
+		
+	}
+	
+	public static int dfs(int[] numbers, int target, int depth, int sum) {
+		if(depth == numbers.length) {
+			if(sum == target)
+				return 1;
+			return 0;
+		}
+		return dfs(numbers, target, depth+1, sum + numbers[depth]) +
+				dfs(numbers, target, depth+1, sum - numbers[depth]);
+	}
+
+}
