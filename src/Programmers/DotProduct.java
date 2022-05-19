9e039fb9910f26f0fcc4f4c556d92593464934a1
PARKJUYOUNG <juzero98@naver.com>
2022-05-19 17:07:34 +0900

    solve Programmers level1-내적

---
+++ src/Programmers/DotProduct.java
@@ -0,0 +1,16 @@
+package Programmers;
+
+public class DotProduct {
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 3, 4 };
+		int[] b = { -3, -1, 0, 2 };
+		
+		int result = 0;
+		for(int i = 0; i < a.length; i++) {
+			result += (a[i] * b[i]);
+		}
+		
+		System.out.println(result);
+	}
+
+}
